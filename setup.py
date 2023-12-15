b93b1d9e3e04e8e18b54f75805e97c84b06d00c6
Collins (Bot) <134794051+comony@users.noreply.github.com>
2023-12-15 04:55:15 +0000

    makin a new release

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ import os
 here = os.path.abspath(os.path.dirname(__file__))
 
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    DESCRIPTION = "\n" + fh.read()
+    long_description = "\n" + fh.read()
 
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 DESCRIPTION = 'PyTextBin is a versatile Python library facilitating seamless conversion between text, binary, JSON, base64, xml and CSV formats with ease.'
 
 # Setting up
@@ -15,11 +15,13 @@ setup(
     name="PyTextBin",
     version=VERSION,
     author="Collins O. Odhiambo",
-    author_email="<support@comon.tech>",
+    author_email="<comon928@gmail.com>",
     description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
-    keywords=['python', 'csv', 'xmml', 'base64', 'text', 'json', 'binary'],
+    install_requires=[''],
+    keywords=['PyTextBin', 'csv', 'xmml', 'base64', 'text', 'json', 'binary'],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
